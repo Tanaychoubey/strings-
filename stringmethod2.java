743954e1c6f10c5550ab3358f916e1173cd64487
Tanaychoubey <76908226+Tanaychoubey@users.noreply.github.com>
2021-01-03 20:36:17 +0530

    Create stringmethod2.java

---
+++ stringmethod2.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // create first string
+    String Name = "tanay ";
+    System.out.println("First String: " + Name);
+
+    // create second
+    String Title = "choubey";
+    System.out.println("Second String: " + Title);
+
+    // join two strings
+    String fullName = Name.concat(Title);
+    System.out.println("Joined String: " + fullName);
+  }
+}
