6e97c93f05aa905438cb4dc0d25343af260d0521
Tanaychoubey <76908226+Tanaychoubey@users.noreply.github.com>
2021-01-03 20:34:21 +0530

    Create methods1.java

---
+++ methods1.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    // create strings
+    String first = "tanay";
+    String second = "choubey";
+
+    // print strings
+    System.out.println(first);   // print tanay
+    System.out.println(second);  // print choubey
+  }
+}
