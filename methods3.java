aab5c0b082264319bb02800e0077ac2a216b5482
Tanaychoubey <76908226+Tanaychoubey@users.noreply.github.com>
2021-01-03 20:37:28 +0530

    Create methods3.java

---
+++ methods3.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    String str1 = "tanay choubey";
+    String str2 = "Java123";
+
+    // convert to upper case letters
+    System.out.println(str1.toUpperCase()); // "TANAY CHOUBEY"
+    System.out.println(str2.toUpperCase()); // "JAVA123"
+  }
+}
